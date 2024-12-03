af7d8c90e872c1817448602cffe47ecbe86c0555
Dimas Maulana Ahmad <97804178+dimasmaulana-ach@users.noreply.github.com>
2024-12-03 20:41:11 +0700

    update structure

---
+++ Driver.cpp
@@ -0,0 +1,12 @@
+
+#include "Structure.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void createListDriver(listDriver &Ld) {
+    first(Ld)= nullptr;
+    last(Ld)=nullptr;
+}
+
--- Structure.h
+++ Structure.h
@@ -12,6 +12,14 @@
 
 using namespace std;
 
+
+// Define
+#define first(L) L.first
+#define last(L) L.last
+#define next(P) P->next
+#define prev(P) P->prev
+#define info(P) P->info
+
 // Driver
 
 struct Driver {
@@ -21,10 +29,100 @@ struct Driver {
     string vehiclePlate;
     string status;
     Driver* next;
+};
 
+// typedef
+typedef Driver DriverInfotype;
+typedef struct elmDriver *addrDriver;
+
+struct elmDriver {
+    DriverInfotype info;
+    addrDriver next;
+};
+
+struct listDriver {
+  addrDriver first;
+  addrDriver last;
 };
 
 // Customer
 
+struct Customer {
+    int id;
+    string name;
+    string phone;
+    string address;
+    Customer* next;
+    Customer* prev;
+};
+
+// typedef
+typedef Customer CustomerInfotype;
+typedef struct elmCustomer* CustomerAddress;
+
+struct elmCustomer {
+    CustomerInfotype info;
+    CustomerAddress next;
+    CustomerAddress prev;
+};
+
+struct listCustomer {
+  CustomerAddress first;
+  CustomerAddress last;
+};
+
 
 // Relation (Order)
+
+struct Relation {
+    Driver* driver;
+    Customer* customer;
+    Relation* next;
+    Relation* prev;
+};
+
+// typedef
+typedef Relation RelationInfotype;
+typedef struct elmRelation* RelationAddress;
+
+struct elmRelation {
+    RelationInfotype info;
+    RelationAddress next;
+    RelationAddress prev;
+};
+
+struct listRelation {
+  RelationAddress first;
+  RelationAddress last;
+};
+
+
+// Create List
+void createListDriver(listDriver &Ld);
+
+/**
+ * ====================================================================================================
+ * Define Kelas Driver
+ * ====================================================================================================
+ */
+
+class DriverClass {
+private:
+    addrDriver head;
+public:
+    DriverClass();
+
+    addrDriver createElementDriver(DriverInfotype data);
+
+    void insertDriver(listDriver &Ld, addrDriver addr_d); // Define Insert Driver
+
+    void deleteDriver(listDriver &Ld, listRelation &Lr, int driverId); // Delete Driver
+
+    addrDriver findDriver(listDriver &Ld, int driverId); // Cari Driver
+
+    void displayDrivers(listDriver &Ld); // Tampilkan List Driver
+
+    int countOrdersPerDriver(listRelation Lr, string driverId ); // Validate and Check Count Order of Driver
+
+    ~DriverClass();
+};
